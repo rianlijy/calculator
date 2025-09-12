92f77346bec327428a9282fe5957191440668ce2
Adrian <adrianleejoonyin@gmail.com>
2025-09-12 18:20:53 +0800

    Add decimals, design and deal with edge case

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ let first = "";
 let second = "";
 let operator = "";
 let sum = 0;
-let firstTime = true;
+let firstCalc = true;
+let firstPeriod = true;
 
 function add(num1, num2){
     return num1 + num2;
@@ -40,20 +41,31 @@ function reset(){
     second = "";
     operator = "";
     sum = 0;
-    firstTime = true;
+    firstCalc = true;
 }
 
 const btn = document.querySelector(".buttons");
 const display = document.querySelector(".calcDisplay");
 btn.addEventListener("click", (event)=>{
     const buttonText = event.target.textContent;
-    if(!isNaN(buttonText)){
+    if(!isNaN(buttonText) || buttonText === "."){
+        if(buttonText === "."){
+            if(firstPeriod === false){
+                return;
+            }
+            firstPeriod = false;
+        }
         second += buttonText;
         display.textContent = second;
     }
     else if(buttonText === "+" || buttonText === "-"|| buttonText === "x"|| buttonText === "/"){
-        if(firstTime === false){
-            sum = operate(Number(first), Number(second), operator);
+        if(firstCalc === false){
+            if(second === "0"){
+                display.textContent = "Nice Try!";
+                reset();
+                return;
+            }
+            sum = Math.round(operate(Number(first), Number(second), operator)*1000)/1000;
             display.textContent = sum;
             first = sum;
         }else{
@@ -62,14 +74,23 @@ btn.addEventListener("click", (event)=>{
         }
         second = "";
         operator = buttonText;
-        firstTime = false;
+        firstCalc = false;
     }
     else if(buttonText === "CLEAR"){
         display.textContent = "";
         reset();
     }
     else if(buttonText === "="){
-        sum = operate(Number(first), Number(second), operator);
+        if(second === "0"){
+            display.textContent = "Nice Try!";
+            reset();
+            return;
+        }
+        if(first === ""){
+            display.textContent = second;
+            return;
+        }
+        sum = Math.round(operate(Number(first), Number(second), operator)*1000)/1000;
         display.textContent = sum;
         reset();
     }
